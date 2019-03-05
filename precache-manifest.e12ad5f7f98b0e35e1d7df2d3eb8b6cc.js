d567026edb9a84f3d783c9fe5f53b06fff59a911
Valeria Gora <valeriia308@gmail.com>
2019-03-05 18:09:51 +0300

    Updates

---
+++ precache-manifest.e12ad5f7f98b0e35e1d7df2d3eb8b6cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todo-list/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "14e46cb4c53468dced63",
+    "url": "/react-todo-list/static/js/runtime~main.14e46cb4.js"
+  },
+  {
+    "revision": "5bb0f11697865766cc13",
+    "url": "/react-todo-list/static/js/main.5bb0f116.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/react-todo-list/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5bb0f11697865766cc13",
+    "url": "/react-todo-list/static/css/main.5c4697c0.chunk.css"
+  },
+  {
+    "revision": "cade4f9fabbe0fafe795b8c48b192419",
+    "url": "/react-todo-list/index.html"
+  }
+];
